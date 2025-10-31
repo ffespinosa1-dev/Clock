f54217194dfea31a2ecfd35db20897d11ce15b2a
ffespinosa1-dev <ffespinosa1@gmail.com>
2025-10-30 23:01:02 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('analog-clock-v2').then(cache => {
+    return cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './sw.js',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]);
+  }));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
